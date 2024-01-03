bd7fcc39056178e072fc8e7240dcae0e29ee36ac
MooseBotter <MooseBotter@users.noreply.github.com>
2024-01-03 17:15:50 +0000

    Auto commit by GitHub Actions Workflow

--- Moose_Include_Static/Moose_.lua
+++ Moose_Include_Static/Moose_.lua
@@ -1,4 +1,4 @@
-env.info('*** MOOSE GITHUB Commit Hash ID: 2024-01-03T18:06:21+01:00-29414bfd21484bddd6eba36d77de5c0875563a57 ***')
+env.info('*** MOOSE GITHUB Commit Hash ID: 2024-01-03T18:14:12+01:00-f57beab6d2f0735469ce2b2e95cdcdb9d911d0b6 ***')
 ModuleLoader='Scripts/Moose/Modules.lua'
 local f=io.open(ModuleLoader,"r")
 if f~=nil then
@@ -55647,6 +55647,710 @@ function TIRESIAS:onafterStop(From,Event,To)
 self:T({From,Event,To})
 self:UnHandleEvent(EVENTS.PlayerEnterAircraft)
 return self
+end
+STRATEGO={
+ClassName="STRATEGO",
+debug=false,
+drawzone=false,
+markzone=false,
+version="0.1.0",
+portweight=3,
+POIweight=1,
+maxrunways=3,
+coalition=nil,
+colors=nil,
+airbasetable={},
+nonconnectedab={},
+easynames={},
+maxdist=150,
+disttable={},
+routexists={},
+routefactor=5,
+OpsZones={},
+NeutralBenefit=100,
+Budget=0,
+usebudget=false,
+CaptureUnits=3,
+CaptureThreatlevel=1,
+}
+STRATEGO.Type={
+AIRBASE="AIRBASE",
+PORT="PORT",
+POI="POI",
+FARP="FARP",
+SHIP="SHIP",
+}
+function STRATEGO:New(Name,Coalition,MaxDist)
+local self=BASE:Inherit(self,BASE:New())
+self.coalition=Coalition
+self.coalitiontext=UTILS.GetCoalitionName(Coalition)
+self.name=Name or"Hannibal"
+self.maxdist=MaxDist or 150
+self.disttable={}
+self.routexists={}
+self.lid=string.format("STRATEGO %s %s | ",self.name,self.version)
+self.bases=SET_AIRBASE:New():FilterOnce()
+self.ports=SET_ZONE:New():FilterPrefixes("Port"):FilterOnce()
+self.POIs=SET_ZONE:New():FilterPrefixes("POI"):FilterOnce()
+self.colors={
+[1]={0,1,0},
+[2]={1,0,0},
+[3]={0,0,1},
+[4]={1,0.65,0},
+}
+return self
+end
+function STRATEGO:Start()
+self:T(self.lid.."Start")
+self:AnalyseBases()
+self:AnalysePOIs(self.ports,self.portweight,"PORT")
+self:AnalysePOIs(self.POIs,self.POIweight,"POI")
+for i=self.maxrunways,1,-1 do
+self:AnalyseRoutes(i,i*self.routefactor,self.colors[(i%3)+1],i)
+end
+self:AnalyseUnconnected(self.colors[4])
+self:I(self.lid.."Advisory ready.")
+return self
+end
+function STRATEGO:SetUsingBudget(Usebudget,StartBudget)
+self:T(self.lid.."SetUsingBudget")
+self.usebudget=Usebudget
+self.Budget=StartBudget
+return self
+end
+function STRATEGO:SetDebug(Debug,DrawZones,MarkZones)
+self.debug=Debug
+self.drawzone=DrawZones
+self.markzone=MarkZones
+return self
+end
+function STRATEGO:SetWeights(MaxRunways,PortWeight,POIWeight,RouteFactor)
+self.portweight=PortWeight or 3
+self.POIweight=POIWeight or 1
+self.maxrunways=MaxRunways or 3
+self.routefactor=RouteFactor or 5
+return self
+end
+function STRATEGO:SetNeutralBenefit(NeutralBenefit)
+self.NeutralBenefit=NeutralBenefit or 100
+return self
+end
+function STRATEGO:SetCaptureOptions(CaptureUnits,CaptureThreatlevel)
+self.CaptureUnits=CaptureUnits or 3
+self.CaptureThreatlevel=CaptureThreatlevel or 1
+return self
+end
+function STRATEGO:AnalyseBases()
+self:T(self.lid.."AnalyseBases")
+local colors=self.colors
+local debug=self.debug
+local airbasetable=self.airbasetable
+local nonconnectedab=self.nonconnectedab
+local easynames=self.easynames
+self.bases:ForEach(
+function(afb)
+local ab=afb
+local abname=ab:GetName()
+local runways=ab:GetRunways()
+local numrwys=#runways
+if numrwys>=1 then numrwys=numrwys*0.5 end
+local abzone=ab:GetZone()
+local coa=ab:GetCoalition()+1
+local abtype="AIRBASE"
+if ab:IsShip()then
+numrwys=1
+abtype="SHIP"
+end
+if ab:IsHelipad()then
+numrwys=1
+abtype="FARP"
+end
+local coord=abzone:GetCoordinate()
+if debug then
+abzone:DrawZone(-1,colors[coa],1,colors[coa],0.3,1)
+coord:TextToAll(tostring(numrwys),-1,{0,0,0},1,colors[coa],0.3,20)
+end
+local opszone=self:GetNewOpsZone(abname,coa-1)
+local tbl={
+name=abname,
+baseweight=numrwys,
+weight=0,
+coalition=coa-1,
+port=false,
+zone=abzone,
+coord=coord,
+type=abtype,
+}
+airbasetable[abname]=tbl
+nonconnectedab[abname]=true
+local name=string.gsub(abname,"[%p%s]",".")
+easynames[name]=abname
+end
+)
+return self
+end
+function STRATEGO:UpdateKnotCoalitions()
+self:T(self.lid.."UpdateKnotCoalitions")
+local newtable={}
+for _id,_data in pairs(self.airbasetable)do
+local data=_data
+if data.type=="AIRBASE"or data.type=="FARP"then
+data.coalition=AIRBASE:FindByName(data.name):GetCoalition()
+else
+data.coalition=data.opszone:GetOwner()
+end
+newtable[_id]=_data
+end
+self.airbasetable=nil
+self.airbasetable=newtable
+return self
+end
+function STRATEGO:GetNewOpsZone(Zone,Coalition)
+self:T(self.lid.."GetNewOpsZone")
+local opszone=OPSZONE:New(Zone,Coalition or 0)
+opszone:SetCaptureNunits(self.CaptureUnits)
+opszone:SetCaptureThreatlevel(self.CaptureThreatlevel)
+opszone:SetDrawZone(self.drawzone)
+opszone:SetMarkZone(self.markzone)
+opszone:Start()
+return opszone
+end
+function STRATEGO:AnalysePOIs(Set,Weight,Key)
+self:T(self.lid.."AnalysePOIs")
+local colors=self.colors
+local debug=self.debug
+local airbasetable=self.airbasetable
+local nonconnectedab=self.nonconnectedab
+local easynames=self.easynames
+Set:ForEach(
+function(port)
+local zone=port
+local zname=zone:GetName()
+local coord=zone:GetCoordinate()
+if debug then
+zone:DrawZone(-1,colors[1],1,colors[1],0.3,1)
+coord:TextToAll(tostring(Weight),-1,{0,0,0},1,colors[1],0.3,20)
+end
+local opszone=self:GetNewOpsZone(zone)
+local tbl={
+name=zname,
+baseweight=Weight,
+weight=0,
+coalition=coalition.side.NEUTRAL,
+port=true,
+zone=zone,
+coord=coord,
+type=Key,
+opszone=opszone,
+}
+airbasetable[zone:GetName()]=tbl
+nonconnectedab[zone:GetName()]=true
+local name=string.gsub(zname,"[%p%s]",".")
+easynames[name]=zname
+end
+)
+return self
+end
+function STRATEGO:GetToFrom(StartPoint,EndPoint)
+self:T(self.lid.."GetToFrom")
+local pstart=string.gsub(StartPoint,"[%p%s]",".")
+local pend=string.gsub(EndPoint,"[%p%s]",".")
+local fromto=pstart..";"..pend
+local tofrom=pend..";"..pstart
+return fromto,tofrom
+end
+function STRATEGO:AddRoutesManually(Startpoint,Endpoint,Color,Linetype,Draw)
+local fromto,tofrom=self:GetToFrom(Startpoint,Endpoint)
+local startcoordinate=self.airbasetable[Startpoint].coord
+local targetcoordinate=self.airbasetable[Endpoint].coord
+local dist=UTILS.Round(targetcoordinate:Get2DDistance(startcoordinate),-2)/1000
+local color=Color or{136/255,0,1}
+local linetype=Linetype or 5
+local data={
+start=Startpoint,
+target=Endpoint,
+dist=dist,
+}
+self.disttable[fromto]=data
+self.disttable[tofrom]=data
+table.insert(self.routexists,fromto)
+table.insert(self.routexists,tofrom)
+self.nonconnectedab[Endpoint]=false
+self.nonconnectedab[Startpoint]=false
+local factor=self.airbasetable[Startpoint].baseweight*self.routefactor
+self.airbasetable[Startpoint].weight=self.airbasetable[Startpoint].weight+factor
+self.airbasetable[Endpoint].weight=self.airbasetable[Endpoint].weight+factor
+if self.debug or Draw then
+startcoordinate:LineToAll(targetcoordinate,-1,color,1,linetype,nil,string.format("%dkm",dist))
+end
+return self
+end
+function STRATEGO:AnalyseRoutes(tgtrwys,factor,color,linetype)
+self:T(self.lid.."AnalyseRoutes")
+for _,_ab in pairs(self.airbasetable)do
+if _ab.baseweight>=1 then
+local startpoint=_ab.name
+local startcoord=_ab.coord
+for _,_data in pairs(self.airbasetable)do
+local fromto,tofrom=self:GetToFrom(startpoint,_data.name)
+if _data.name==startpoint then
+elseif _data.baseweight==tgtrwys and not(self.routexists[fromto]or self.routexists[tofrom])then
+local tgtc=_data.coord
+local dist=UTILS.Round(tgtc:Get2DDistance(startcoord),-2)/1000
+if dist<=self.maxdist then
+local data={
+start=startpoint,
+target=_data.name,
+dist=dist,
+}
+self.disttable[fromto]=data
+self.disttable[tofrom]=data
+table.insert(self.routexists,fromto)
+table.insert(self.routexists,tofrom)
+self.nonconnectedab[_data.name]=false
+self.nonconnectedab[startpoint]=false
+self.airbasetable[startpoint].weight=self.airbasetable[startpoint].weight+factor
+self.airbasetable[_data.name].weight=self.airbasetable[_data.name].weight+factor
+if self.debug then
+startcoord:LineToAll(tgtc,-1,color,1,linetype,nil,string.format("%dkm",dist))
+end
+end
+end
+end
+end
+end
+return self
+end
+function STRATEGO:AnalyseUnconnected(Color)
+self:T(self.lid.."AnalyseUnconnected")
+for _name,_noconnect in pairs(self.nonconnectedab)do
+if _noconnect then
+local startpoint=_name
+local startcoord=self.airbasetable[_name].coord
+local shortest=1000*1000
+local closest=nil
+local closestcoord=nil
+for _,_data in pairs(self.airbasetable)do
+if _name~=_data.name then
+local tgtc=_data.coord
+local dist=UTILS.Round(tgtc:Get2DDistance(startcoord),-2)/1000
+if dist<shortest and self.nonconnectedab[_data.name]==false then
+shortest=dist
+closest=_data.name
+closestcoord=tgtc
+end
+end
+end
+if closest then
+if self.debug then
+startcoord:LineToAll(closestcoord,-1,Color,1,3,nil,string.format("%dkm",shortest))
+end
+self.airbasetable[startpoint].weight=self.airbasetable[startpoint].weight+1
+self.airbasetable[closest].weight=self.airbasetable[closest].weight+1
+local data={
+start=startpoint,
+target=closest,
+dist=shortest,
+}
+local fromto,tofrom=self:GetToFrom(startpoint,closest)
+self.disttable[fromto]=data
+self.disttable[tofrom]=data
+table.insert(self.routexists,fromto)
+table.insert(self.routexists,tofrom)
+end
+end
+end
+return self
+end
+function STRATEGO:GetHighestWeightKnots()
+self:T(self.lid.."GetHighestWeightBases")
+local weight=0
+local airbases={}
+for _name,_data in pairs(self.airbasetable)do
+if _data.weight>=weight then
+weight=_data.weight
+if not airbases[weight]then airbases[weight]={}end
+table.insert(airbases[weight],_name)
+end
+end
+return airbases[weight],weight
+end
+function STRATEGO:GetNextHighestWeightKnots(Weight)
+self:T(self.lid.."GetNextHighestWeightBases")
+local weight=0
+local airbases={}
+for _name,_data in pairs(self.airbasetable)do
+if _data.weight>=weight and _data.weight<Weight then
+weight=_data.weight
+if not airbases[weight]then airbases[weight]={}end
+table.insert(airbases[weight],_name)
+end
+end
+return airbases[weight],weight
+end
+function STRATEGO:GetKnotWeight(Name)
+if Name and self.airbasetable[Name]then
+return self.airbasetable[Name].weight or 0
+else
+return 0
+end
+end
+function STRATEGO:GetKnotBaseWeight(Name)
+if Name and self.airbasetable[Name]then
+return self.airbasetable[Name].baseweight or 0
+else
+return 0
+end
+end
+function STRATEGO:GetKnotCoalition(Name)
+if Name and self.airbasetable[Name]then
+return self.airbasetable[Name].coalition or coalition.side.NEUTRAL
+else
+return coalition.side.NEUTRAL
+end
+end
+function STRATEGO:GetKnotType(Name)
+if Name and self.airbasetable[Name]then
+return self.airbasetable[Name].type
+else
+return nil
+end
+end
+function STRATEGO:GetKnotZone(Name)
+if Name and self.airbasetable[Name]then
+return self.airbasetable[Name].zone
+else
+return nil
+end
+end
+function STRATEGO:GetKnotOpsZone(Name)
+if Name and self.airbasetable[Name]then
+return self.airbasetable[Name].opszone
+else
+return nil
+end
+end
+function STRATEGO:GetKnotCoordinate(Name)
+if Name and self.airbasetable[Name]then
+return self.airbasetable[Name].coord
+else
+return nil
+end
+end
+function STRATEGO:IsAirbase(Name)
+if Name and self.airbasetable[Name]then
+return self.airbasetable[Name].type==STRATEGO.Type.AIRBASE
+else
+return false
+end
+end
+function STRATEGO:IsPort(Name)
+if Name and self.airbasetable[Name]then
+return self.airbasetable[Name].type==STRATEGO.Type.PORT
+else
+return false
+end
+end
+function STRATEGO:IsPOI(Name)
+if Name and self.airbasetable[Name]then
+return self.airbasetable[Name].type==STRATEGO.Type.POI
+else
+return false
+end
+end
+function STRATEGO:IsFARP(Name)
+if Name and self.airbasetable[Name]then
+return self.airbasetable[Name].type==STRATEGO.Type.FARP
+else
+return false
+end
+end
+function STRATEGO:IsShip(Name)
+if Name and self.airbasetable[Name]then
+return self.airbasetable[Name].type==STRATEGO.Type.SHIP
+else
+return false
+end
+end
+function STRATEGO:FindClosestConsolidationTarget(Startpoint,BaseWeight)
+self:T(self.lid.."FindClosestConsolidationTarget for "..Startpoint.." Weight "..BaseWeight or 0)
+local shortest=1000*1000
+local target=nil
+local weight=0
+local coa=nil
+if not BaseWeight then BaseWeight=self.maxrunways-1 end
+local startpoint=string.gsub(Startpoint,"[%p%s]",".")
+for _,_route in pairs(self.routexists)do
+if string.find(_route,startpoint,1,true)then
+local dist=self.disttable[_route].dist
+local tname=string.gsub(_route,startpoint,"")
+local tname=string.gsub(tname,";","")
+local cname=self.easynames[tname]
+local targetweight=self.airbasetable[cname].baseweight
+coa=self.airbasetable[cname].coalition
+if(dist<shortest)and(coa~=self.coalition)and(BaseWeight>=targetweight)then
+shortest=dist
+target=cname
+weight=self.airbasetable[cname].weight
+coa=self.airbasetable[cname].coalition
+end
+end
+end
+return shortest,target,weight,coa
+end
+function STRATEGO:FindClosestStrategicTarget(Startpoint,BaseWeight)
+self:T(self.lid.."FindClosestStrategicTarget")
+local shortest=1000*1000
+local target=nil
+local weight=0
+local coa=nil
+if not BaseWeight then BaseWeight=self.maxrunways end
+local startpoint=string.gsub(Startpoint,"[%p%s]",".")
+for _,_route in pairs(self.routexists)do
+if string.find(_route,startpoint,1,true)then
+local dist=self.disttable[_route].dist
+local tname=string.gsub(_route,startpoint,"")
+local tname=string.gsub(tname,";","")
+local cname=self.easynames[tname]
+if dist<shortest and self.airbasetable[cname].coalition~=self.coalition and self.airbasetable[cname].baseweight>=BaseWeight then
+shortest=dist
+target=cname
+weight=self.airbasetable[cname].weight
+coa=self.airbasetable[cname].coalition
+end
+end
+end
+return shortest,target,weight,coa
+end
+function STRATEGO:FindStrategicTargets()
+local targets={}
+for _,_data in pairs(self.airbasetable)do
+local data=_data
+if data.coalition==self.coalition then
+local dist,name,points,coa=self:FindClosestStrategicTarget(data.name,self.maxrunways)
+if coa==coalition.side.NEUTRAL and points~=0 then
+local fpoints=points+self.NeutralBenefit
+local tries=1
+while targets[fpoints]or tries<100 do
+fpoints=points+(self.NeutralBenefit+math.random(1,100))
+tries=tries+1
+end
+targets[fpoints]={
+name=name,
+dist=dist,
+points=fpoints,
+coalition=coa,
+}
+end
+local enemycoa=self.coalition==coalition.side.BLUE and coalition.side.RED or coalition.side.BLUE
+if coa==enemycoa and points~=0 then
+local fpoints=points
+local tries=1
+while targets[fpoints]or tries<100 do
+fpoints=points+(math.random(1,100))
+tries=tries+1
+end
+targets[fpoints]={
+name=name,
+dist=dist,
+points=fpoints,
+coalition=coa,
+}
+end
+end
+end
+return targets
+end
+function STRATEGO:FindConsolidationTargets()
+local targets={}
+for _,_data in pairs(self.airbasetable)do
+local data=_data
+if data.coalition==self.coalition then
+local dist,name,points,coa=self:FindClosestConsolidationTarget(data.name,self.maxrunways-1)
+if coa==coalition.side.NEUTRAL and points~=0 then
+local fpoints=points+self.NeutralBenefit
+local tries=1
+while targets[fpoints]or tries<100 do
+fpoints=points-(self.NeutralBenefit+math.random(1,100))
+tries=tries+1
+end
+targets[fpoints]={
+name=name,
+dist=dist,
+points=fpoints,
+coalition=coa,
+coalitionname=UTILS.GetCoalitionName(coa),
+}
+end
+local enemycoa=self.coalition==coalition.side.BLUE and coalition.side.RED or coalition.side.BLUE
+if coa==enemycoa and points~=0 then
+local fpoints=points
+local tries=1
+while targets[fpoints]or tries<100 do
+fpoints=points-(math.random(1,100))
+tries=tries+1
+end
+targets[fpoints]={
+name=name,
+dist=dist,
+points=fpoints,
+coalition=coa,
+coalitionname=UTILS.GetCoalitionName(coa),
+}
+end
+end
+end
+return targets
+end
+function STRATEGO:FindNeighborKnots(Name,Enemies,Friends)
+local neighbors={}
+local name=string.gsub(Name,"[%p%s]",".")
+for _route,_data in pairs(self.disttable)do
+if string.find(_route,name,1,true)then
+local dist=self.disttable[_route]
+local tname=string.gsub(_route,name,"")
+local tname=string.gsub(tname,";","")
+local cname=self.easynames[tname]
+local encoa=self.coalition==coalition.side.BLUE and coalition.side.RED or coalition.side.BLUE
+if Enemies==true then
+if self.airbasetable[cname].coalition==encoa then
+neighbors[cname]=dist
+end
+elseif Friends==true then
+if self.airbasetable[cname].coalition~=encoa then
+neighbors[cname]=dist
+end
+else
+neighbors[cname]=dist
+end
+end
+end
+return neighbors
+end
+function STRATEGO:FindRoute(Start,End,Hops,Draw)
+self:I({Start,End,Hops})
+local Route={}
+local hops=Hops or 4
+local routecomplete=false
+local function Checker(neighbors)
+for _name,_data in pairs(neighbors)do
+if _name==End then
+return End
+end
+end
+return nil
+end
+local function NextClosest(Start,End)
+local ecoord=self.airbasetable[End].coord
+local knots=self:FindNeighborKnots(Start)
+local closest=nil
+local closedist=1000*1000
+for _name,_dist in pairs(knots)do
+local kcoord=self.airbasetable[_name].coord
+local dist=math.floor((kcoord:Get2DDistance(ecoord)/1000)+0.5)
+if dist<closedist then
+closedist=dist
+closest=_name
+end
+end
+if closest then
+return closest
+end
+end
+local function DrawRoute(Route)
+for i=1,#Route-1 do
+local p1=Route[i]
+local p2=Route[i+1]
+local c1=self.airbasetable[p1].coord
+local c2=self.airbasetable[p2].coord
+c1:LineToAll(c2,-1,{0,0,0},1,6)
+end
+end
+Route[#Route+1]=Start
+local knots=self:FindNeighborKnots(Start)
+local endpoint=Checker(knots)
+if endpoint then
+Route[#Route+1]=endpoint
+routecomplete=true
+else
+local spoint=Start
+for i=1,hops do
+local Next=NextClosest(spoint,End)
+if Next then
+Route[#Route+1]=Next
+local knots=self:FindNeighborKnots(Next)
+local endpoint=Checker(knots)
+if endpoint then
+Route[#Route+1]=endpoint
+routecomplete=true
+break
+else
+spoint=Next
+end
+end
+end
+end
+if self.debug or Draw then DrawRoute(Route)end
+return Route,routecomplete
+end
+function STRATEGO:AddBudget(Number)
+self.Budget=self.Budget+Number
+return self
+end
+function STRATEGO:SubtractBudget(Number)
+self.Budget=self.Budget-Number
+return self
+end
+function STRATEGO:GetBudget()
+return self.Budget
+end
+function STRATEGO:FindAffordableStrategicTarget()
+local targets=self:FindStrategicTargets()
+local budget=self.Budget
+local target=nil
+local Targets={}
+for _,_data in pairs(targets)do
+local data=_data
+if data.points<=budget then
+table.insert(Targets,data)
+self:T(self.lid.."Affordable strategic target: "..data.name)
+end
+end
+if not targets then
+self:T(self.lid.."No suitable target found!")
+return nil
+end
+target=Targets[math.random(1,#Targets)]
+if target then
+self:T(self.lid.."Final affordable strategic target: "..target.name)
+return target
+else
+return nil
+end
+end
+function STRATEGO:FindAffordableConsolidationTarget()
+local targets=self:FindConsolidationTargets()
+local budget=self.Budget
+local target=nil
+local Targets={}
+for _,_data in pairs(targets)do
+local data=_data
+if data.points<=budget then
+table.insert(Targets,data)
+self:T(self.lid.."Affordable consolidation target: "..data.name)
+end
+end
+if not targets then
+self:T(self.lid.."No suitable target found!")
+return nil
+end
+target=Targets[math.random(1,#Targets)]
+if target then
+self:T(self.lid.."Final affordable consolidation target: "..target.name)
+return target
+else
+return nil
+end
 end
 AIRBOSS={
 ClassName="AIRBOSS",
