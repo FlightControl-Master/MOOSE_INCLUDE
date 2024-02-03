dea4ca62c4e203f2ff6db3fe38c7dccdecc72c78
MooseBotter <MooseBotter@users.noreply.github.com>
2024-02-03 08:12:20 +0000

    Auto commit by GitHub Actions Workflow

--- Moose_Include_Static/Moose_.lua
+++ Moose_Include_Static/Moose_.lua
@@ -1,4 +1,4 @@
-env.info('*** MOOSE GITHUB Commit Hash ID: 2024-01-31T17:49:59+01:00-531132e8a733dfb2b93c622605f18565cf6241a2 ***')
+env.info('*** MOOSE GITHUB Commit Hash ID: 2024-02-03T09:10:41+01:00-48dba742ad0522aad0a8e4f15d8c739e48f5b7a1 ***')
 if not MOOSE_DEVELOPMENT_FOLDER then
 MOOSE_DEVELOPMENT_FOLDER='Scripts'
 end
@@ -51543,9 +51543,9 @@ MEDIUM="Medium",
 LONG="Long",
 }
 MANTIS.SamData={
-["Hawk"]={Range=44,Blindspot=0,Height=9,Type="Medium",Radar="Hawk"},
-["NASAMS"]={Range=14,Blindspot=0,Height=3,Type="Short",Radar="NSAMS"},
-["Patriot"]={Range=99,Blindspot=0,Height=9,Type="Long",Radar="Patriot"},
+["Hawk"]={Range=44,Blindspot=0,Height=12,Type="Medium",Radar="Hawk"},
+["NASAMS"]={Range=14,Blindspot=0,Height=8,Type="Short",Radar="NSAMS"},
+["Patriot"]={Range=99,Blindspot=0,Height=25,Type="Long",Radar="Patriot"},
 ["Rapier"]={Range=6,Blindspot=0,Height=3,Type="Short",Radar="rapier"},
 ["SA-2"]={Range=40,Blindspot=7,Height=25,Type="Medium",Radar="S_75M_Volhov"},
 ["SA-3"]={Range=18,Blindspot=6,Height=18,Type="Short",Radar="5p73 s-125 ln"},
@@ -51553,7 +51553,7 @@ MANTIS.SamData={
 ["SA-6"]={Range=25,Blindspot=0,Height=8,Type="Medium",Radar="1S91"},
 ["SA-10"]={Range=119,Blindspot=0,Height=18,Type="Long",Radar="S-300PS 4"},
 ["SA-11"]={Range=35,Blindspot=0,Height=20,Type="Medium",Radar="SA-11"},
-["Roland"]={Range=8,Blindspot=0,Height=3,Type="Short",Radar="Roland"},
+["Roland"]={Range=8,Blindspot=0,Height=5,Type="Short",Radar="Roland"},
 ["HQ-7"]={Range=12,Blindspot=0,Height=3,Type="Short",Radar="HQ-7"},
 ["SA-9"]={Range=4,Blindspot=0,Height=3,Type="Short",Radar="Strela"},
 ["SA-8"]={Range=10,Blindspot=0,Height=5,Type="Short",Radar="Osa 9A33"},
