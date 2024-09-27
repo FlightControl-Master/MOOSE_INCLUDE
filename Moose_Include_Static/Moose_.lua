6139e2ab146ecceab242ecbfe09abf62a3080390
MooseBotter <MooseBotter@users.noreply.github.com>
2024-09-27 09:59:49 +0000

    Auto commit by GitHub Actions Workflow

--- Moose_Include_Static/Moose_.lua
+++ Moose_Include_Static/Moose_.lua
@@ -1,4 +1,4 @@
-env.info('*** MOOSE GITHUB Commit Hash ID: 2024-09-26T09:03:05+02:00-1dce2eb747a48ae927ab4186926bee87aae3d285 ***')
+env.info('*** MOOSE GITHUB Commit Hash ID: 2024-09-27T11:58:08+02:00-0d877853ebefc5b0c70c3bf0cb78f4d44171ce54 ***')
 if not MOOSE_DEVELOPMENT_FOLDER then
 MOOSE_DEVELOPMENT_FOLDER='Scripts'
 end
@@ -7489,7 +7489,7 @@ Event.IniCategory=Event.IniDCSUnit:getDesc().category
 Event.IniTypeName=Event.IniDCSUnit:getTypeName()
 elseif Event.IniObjectCategory==Object.Category.SCENERY then
 Event.IniDCSUnit=Event.initiator
-Event.IniDCSUnitName=Event.IniDCSUnit:getName()
+Event.IniDCSUnitName=Event.IniDCSUnit.getName and Event.IniDCSUnit:getName()or"Scenery no name "..math.random(1,20000)
 Event.IniUnitName=Event.IniDCSUnitName
 Event.IniUnit=SCENERY:Register(Event.IniDCSUnitName,Event.initiator)
 Event.IniCategory=Event.IniDCSUnit:getDesc().category
