97b9c947c86d81dd1f1b4047278e3b21077e6922
MooseBotter <MooseBotter@users.noreply.github.com>
2025-08-28 18:00:27 +0000

    Auto commit by GitHub Actions Workflow

--- Moose_Include_Static/Moose_.lua
+++ Moose_Include_Static/Moose_.lua
@@ -1,4 +1,4 @@
-env.info('*** MOOSE GITHUB Commit Hash ID: 2025-08-27T14:23:36+02:00-f1636fc5a911b8ceeb013d0fb7dbcdaca2e141ae ***')
+env.info('*** MOOSE GITHUB Commit Hash ID: 2025-08-28T19:58:19+02:00-297164a0ee2cd4f93e0a3a76563f2f637348f68a ***')
 if not MOOSE_DEVELOPMENT_FOLDER then
 MOOSE_DEVELOPMENT_FOLDER='Scripts'
 end
@@ -4379,6 +4379,43 @@ end
 end
 end
 end
+end
+function UTILS.ValidateAndRepositionStatic(Country,Category,Type,Position,ShapeName,MaxRadius)
+local coord=COORDINATE:NewFromVec2(Position)
+local st=SPAWNSTATIC:NewFromType(Type,Category,Country)
+if ShapeName then
+st:InitShape(ShapeName)
+end
+local sName="s-"..timer.getTime().."-"..math.random(1,10000)
+local tempStatic=st:SpawnFromCoordinate(coord,0,sName)
+if tempStatic then
+local sRadius=tempStatic:GetBoundingRadius(2)or 3
+tempStatic:Destroy()
+sRadius=sRadius*0.5
+MaxRadius=MaxRadius or math.max(sRadius*10,100)
+local positions=UTILS.GetSimpleZones(coord:GetVec3(),MaxRadius,sRadius,20)
+if positions and#positions>0 then
+local closestSpot
+local closestDist=math.huge
+for _,spot in pairs(positions)do
+if land.getSurfaceType(spot)==land.SurfaceType.LAND then
+local dist=UTILS.VecDist2D(Position,spot)
+if dist<closestDist then
+closestDist=dist
+closestSpot=spot
+end
+end
+end
+if closestSpot then
+if closestDist>=sRadius then
+return closestSpot
+else
+return Position
+end
+end
+end
+end
+return nil
 end
 PROFILER={
 ClassName="PROFILER",
@@ -21529,6 +21566,11 @@ self.InitOffsetY=OffsetY or 0
 self.InitOffsetAngle=OffsetAngle or 0
 return self
 end
+function SPAWNSTATIC:InitValidateAndRepositionStatic(OnOff,MaxRadius)
+self.ValidateAndRepositionStatic=OnOff
+self.ValidateAndRepositionStaticMaxRadius=MaxRadius
+return self
+end
 function SPAWNSTATIC:OnSpawnStatic(SpawnCallBackFunction,...)
 self:F("OnSpawnStatic")
 self.SpawnFunctionHook=SpawnCallBackFunction
@@ -21615,6 +21657,13 @@ end
 self.SpawnIndex=self.SpawnIndex+1
 Template.name=self.InitStaticName or string.format("%s#%05d",self.SpawnTemplatePrefix,self.SpawnIndex)
 local Static=nil
+if self.ValidateAndRepositionStatic then
+local validPos=UTILS.ValidateAndRepositionStatic(CountryID,Template.category,Template.type,Template,Template.shape_name,self.ValidateAndRepositionStaticMaxRadius)
+if validPos then
+Template.x=validPos.x
+Template.y=validPos.y
+end
+end
 if self.InitFarp then
 local TemplateGroup={}
 TemplateGroup.units={}
