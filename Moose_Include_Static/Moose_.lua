4c33c7bed583b65aca60a657966b05184969fce4
MooseBotter <MooseBotter@users.noreply.github.com>
2025-10-25 14:59:27 +0000

    Auto commit by GitHub Actions Workflow

--- Moose_Include_Static/Moose_.lua
+++ Moose_Include_Static/Moose_.lua
@@ -1,4 +1,4 @@
-env.info('*** MOOSE GITHUB Commit Hash ID: 2025-10-24T16:27:17+02:00-86798ae9ea7dc803059feb3f367cfedaf0fe1131 ***')
+env.info('*** MOOSE GITHUB Commit Hash ID: 2025-10-25T16:56:16+02:00-7d4e1036602f291330260e47f9c99f3efcfcae81 ***')
 if not MOOSE_DEVELOPMENT_FOLDER then
 MOOSE_DEVELOPMENT_FOLDER='Scripts'
 end
@@ -3959,15 +3959,48 @@ local SpacingY=SpacingY or 100
 local FarpVec2=Coordinate:GetVec2()
 if NumberPads>1 then
 local Grid=UTILS.GenerateGridPoints(FarpVec2,NumberPads,SpacingX,SpacingY)
+local groupData={
+["visible"]=true,
+["hidden"]=false,
+["units"]={},
+["y"]=0,
+["x"]=0,
+["name"]=Name,
+}
+local unitData={
+["category"]="Heliports",
+["type"]=STypeName,
+["y"]=0,
+["x"]=0,
+["name"]=Name,
+["heading"]=0,
+["heliport_modulation"]=mod,
+["heliport_frequency"]=freq,
+["heliport_callsign_id"]=callsign,
+["dead"]=false,
+["shape_name"]=SShapeName,
+["dynamicSpawn"]=DynamicSpawns,
+["allowHotStart"]=HotStart,
+}
 for id,gridpoint in ipairs(Grid)do
-local location=COORDINATE:NewFromVec2(gridpoint)
-local newfarp=SPAWNSTATIC:NewFromType(STypeName,"Heliports",Country)
-newfarp:InitShape(SShapeName)
-newfarp:InitFARP(callsign,freq,mod,DynamicSpawns,HotStart)
-local spawnedfarp=newfarp:SpawnFromCoordinate(location,0,Name.."-"..id)
-table.insert(ReturnObjects,spawnedfarp)
-PopulateStorage(Name.."-"..id,liquids,equip,airframes)
+local UnitTemplate=UTILS.DeepCopy(unitData)
+UnitTemplate.x=gridpoint.x
+UnitTemplate.y=gridpoint.y
+UnitTemplate.name=Name.."-"..id
+table.insert(groupData.units,UnitTemplate)
+if id==1 then
+groupData.x=gridpoint.x
+groupData.y=gridpoint.y
 end
+end
+local Static=coalition.addGroup(Country,-1,groupData)
+local Event={
+id=EVENTS.Birth,
+time=timer.getTime(),
+initiator=Static
+}
+world.onEvent(Event)
+PopulateStorage(Name.."-1",liquids,equip,airframes)
 else
 local newfarp=SPAWNSTATIC:NewFromType(STypeName,"Heliports",Country)
 newfarp:InitShape(SShapeName)
