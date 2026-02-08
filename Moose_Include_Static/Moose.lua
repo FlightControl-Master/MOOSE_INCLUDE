40b062a4467033ac7bb0aa50154ae8b70a0b74ab
MooseBotter <MooseBotter@users.noreply.github.com>
2026-02-08 14:09:25 +0000

    Auto commit by GitHub Actions Workflow

--- Moose_Include_Static/Moose.lua
+++ Moose_Include_Static/Moose.lua
@@ -1,4 +1,4 @@
-env.info( '*** MOOSE GITHUB Commit Hash ID: 2026-02-08T14:16:11+01:00-176d614d86b56fb08d4ad8417f9530d21746d9f0 ***' )
+env.info( '*** MOOSE GITHUB Commit Hash ID: 2026-02-08T15:07:09+01:00-1729c14acc07ec6b3c6e4d82ee8c7f042434ac5b ***' )
 
 -- Automatic dynamic loading of development files, if they exists.
 -- Try to load Moose as individual script files from <DcsInstallDir\Script\Moose
@@ -1850,6 +1850,8 @@ ENUMS.Storage.weapons.missiles.Igla_S = 'weapons.missiles.Igla_S'
 ENUMS.Storage.weapons.gunmounts.AKAN_NO_TRC = 'weapons.gunmounts.{AKAN_NO_TRC}'
 ENUMS.Storage.weapons.gunmounts.AKAN = 'weapons.gunmounts.{AKAN}'
 ENUMS.Storage.weapons.shells.M882_9x19 = 'weapons.shells.9x19_m882'
+ENUMS.Storage.weapons.droptanks.fuel_tank_370gal = "weapons.droptanks.fuel_tank_370gal"
+ENUMS.Storage.weapons.droptanks.fuel_tank_300gal  = "weapons.droptanks.fuel_tank_300gal"
 
 -- UH-60L Mod
 ENUMS.Storage.weapons.gunmounts.UH60LGAU19 = 'weapons.gunmounts.UH-60L GAU-19'
--- Moose_Include_Static/Moose_.lua
+++ Moose_Include_Static/Moose_.lua
@@ -1,4 +1,4 @@
-env.info('*** MOOSE GITHUB Commit Hash ID: 2026-02-08T14:16:11+01:00-176d614d86b56fb08d4ad8417f9530d21746d9f0 ***')
+env.info('*** MOOSE GITHUB Commit Hash ID: 2026-02-08T15:07:09+01:00-1729c14acc07ec6b3c6e4d82ee8c7f042434ac5b ***')
 if not MOOSE_DEVELOPMENT_FOLDER then
 MOOSE_DEVELOPMENT_FOLDER='Scripts'
 end
@@ -1660,6 +1660,8 @@ ENUMS.Storage.weapons.missiles.Igla_S='weapons.missiles.Igla_S'
 ENUMS.Storage.weapons.gunmounts.AKAN_NO_TRC='weapons.gunmounts.{AKAN_NO_TRC}'
 ENUMS.Storage.weapons.gunmounts.AKAN='weapons.gunmounts.{AKAN}'
 ENUMS.Storage.weapons.shells.M882_9x19='weapons.shells.9x19_m882'
+ENUMS.Storage.weapons.droptanks.fuel_tank_370gal="weapons.droptanks.fuel_tank_370gal"
+ENUMS.Storage.weapons.droptanks.fuel_tank_300gal="weapons.droptanks.fuel_tank_300gal"
 ENUMS.Storage.weapons.gunmounts.UH60LGAU19='weapons.gunmounts.UH-60L GAU-19'
 ENUMS.Storage.weapons.gunmounts.UH60L_M134='weapons.gunmounts.UH60L_M134'
 ENUMS.Storage.weapons.gunmounts.UH60_M134='weapons.gunmounts.UH60_M134'
