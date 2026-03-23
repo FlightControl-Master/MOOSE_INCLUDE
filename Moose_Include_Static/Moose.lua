76c0fe581ce4d124d6cbcd348874d3f813b32e79
MooseBotter <MooseBotter@users.noreply.github.com>
2026-03-23 05:20:27 +0000

    Auto commit by GitHub Actions Workflow

--- Moose_Include_Static/Moose.lua
+++ Moose_Include_Static/Moose.lua
@@ -1,4 +1,4 @@
-env.info( '*** MOOSE GITHUB Commit Hash ID: 2026-03-22T17:58:38+01:00-a7127f07cb4c1c261d2797bb284fbe915b9827af ***' )
+env.info( '*** MOOSE GITHUB Commit Hash ID: 2026-03-23T06:18:06+01:00-69784226eabbe4ffc2106dd2905e1d6e0f5fb10b ***' )
 
 -- Automatic dynamic loading of development files, if they exists.
 -- Try to load Moose as individual script files from <DcsInstallDir\Script\Moose
@@ -156424,13 +156424,13 @@ function CTLD:_BuildCrates(Group, Unit,Engineering,MultiDrop,NotifyGroup)
         local distToUnit=Unit and ccoord:Get2DDistance(Unit:GetCoordinate())or 0
         local isHercDrop=Crate:WasDropped(true)
         if not isHercDrop and distToUnit>baseDist then
-      elseif self.UseC130LoadAndUnload and self:IsC130J(Unit) and distToUnit<15 then
+      elseif  self:IsC130J(Unit) and distToUnit<15 then
         -- self:_SendMessage("Please unload crates from the C-130 before building!",10,false,Group)
         -- return self
-      elseif self.UseC130LoadAndUnload and self:IsHook(Unit) and distToUnit<5 then
+      elseif self:IsHook(Unit) and distToUnit<5 then
         -- self:_SendMessage("Please unload crates from the CH-47 before building!",10,false,Group)
         -- return self
-      elseif self.UseC130LoadAndUnload and (Unit:GetTypeName()=="Mi-8MTV2" or Unit:GetTypeName()=="Mi-8MT") and distToUnit<8 then
+      elseif (Unit:GetTypeName()=="Mi-8MTV2" or Unit:GetTypeName()=="Mi-8MT") and distToUnit<8 then
       else
         --local testmarker = ccoord:MarkToAll("Crate found",true,"Build Position")
         if not buildables[name] then
@@ -159487,6 +159487,13 @@ end
 -- @return #number distance Distance to closest zone
 -- @return #number width Radius of zone or width of ship
 function CTLD:IsUnitInZone(Unit,Zonetype)
+  if not Unit then
+    if Zonetype == CTLD.CargoZoneType.SHIP then
+      return false, nil, nil, 1000000, nil
+    else
+      return false, nil, nil, 1000000
+    end
+  end
   self:T(self.lid .. " IsUnitInZone")
   self:T(Zonetype)
   local unitname = Unit:GetName()
@@ -159509,6 +159516,13 @@ function CTLD:IsUnitInZone(Unit,Zonetype)
   local zonewret = nil
   local zonenameret = nil
   local unitcoord = Unit:GetCoordinate()
+  if not unitcoord then
+    if Zonetype == CTLD.CargoZoneType.SHIP then
+      return false, nil, nil, 1000000, nil
+    else
+      return false, nil, nil, 1000000
+    end
+  end
   local unitVec2 = unitcoord:GetVec2()
   for _,_cargozone in pairs(zonetable) do
     local czone = _cargozone -- #CTLD.CargoZone
--- Moose_Include_Static/Moose_.lua
+++ Moose_Include_Static/Moose_.lua
@@ -1,4 +1,4 @@
-env.info('*** MOOSE GITHUB Commit Hash ID: 2026-03-22T17:58:38+01:00-a7127f07cb4c1c261d2797bb284fbe915b9827af ***')
+env.info('*** MOOSE GITHUB Commit Hash ID: 2026-03-23T06:18:06+01:00-69784226eabbe4ffc2106dd2905e1d6e0f5fb10b ***')
 if not MOOSE_DEVELOPMENT_FOLDER then
 MOOSE_DEVELOPMENT_FOLDER='Scripts'
 end
@@ -77390,9 +77390,9 @@ local ccoord=Crate:GetPositionable():GetCoordinate()
 local distToUnit=Unit and ccoord:Get2DDistance(Unit:GetCoordinate())or 0
 local isHercDrop=Crate:WasDropped(true)
 if not isHercDrop and distToUnit>baseDist then
-elseif self.UseC130LoadAndUnload and self:IsC130J(Unit)and distToUnit<15 then
-elseif self.UseC130LoadAndUnload and self:IsHook(Unit)and distToUnit<5 then
-elseif self.UseC130LoadAndUnload and(Unit:GetTypeName()=="Mi-8MTV2"or Unit:GetTypeName()=="Mi-8MT")and distToUnit<8 then
+elseif self:IsC130J(Unit)and distToUnit<15 then
+elseif self:IsHook(Unit)and distToUnit<5 then
+elseif(Unit:GetTypeName()=="Mi-8MTV2"or Unit:GetTypeName()=="Mi-8MT")and distToUnit<8 then
 else
 if not buildables[name]then
 local object={}
@@ -79795,6 +79795,13 @@ end
 return self
 end
 function CTLD:IsUnitInZone(Unit,Zonetype)
+if not Unit then
+if Zonetype==CTLD.CargoZoneType.SHIP then
+return false,nil,nil,1000000,nil
+else
+return false,nil,nil,1000000
+end
+end
 self:T(self.lid.." IsUnitInZone")
 self:T(Zonetype)
 local unitname=Unit:GetName()
@@ -79816,6 +79823,13 @@ local zoneret=nil
 local zonewret=nil
 local zonenameret=nil
 local unitcoord=Unit:GetCoordinate()
+if not unitcoord then
+if Zonetype==CTLD.CargoZoneType.SHIP then
+return false,nil,nil,1000000,nil
+else
+return false,nil,nil,1000000
+end
+end
 local unitVec2=unitcoord:GetVec2()
 for _,_cargozone in pairs(zonetable)do
 local czone=_cargozone
