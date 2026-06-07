be8089d513b1fd1b36221eba83b25b226469e927
MooseBotter <MooseBotter@users.noreply.github.com>
2026-06-07 18:06:33 +0000

    Auto commit by GitHub Actions Workflow

--- Moose_Include_Static/Moose_.lua
+++ Moose_Include_Static/Moose_.lua
@@ -1,4 +1,4 @@
-env.info('*** MOOSE GITHUB Commit Hash ID: 2026-06-05T14:25:02+02:00-27bd27a2e52e08d9b62a57f04d313a1d2afe066b ***')
+env.info('*** MOOSE GITHUB Commit Hash ID: 2026-06-07T20:04:10+02:00-a0420e1cb6d4873f817f56c4ed01834208d6f3c2 ***')
 if not MOOSE_DEVELOPMENT_FOLDER then
 MOOSE_DEVELOPMENT_FOLDER='Scripts'
 end
@@ -56036,38 +56036,32 @@ Mixed={"1xALQ99_1xALQ249","1xALQ99_2xALQ249","2xALQ99_1xALQ249"},
 }
 MANTIS.JammerJitterPercent=0.10
 MANTIS.JammerSAMParams={
-["Nike"]={peak=78,mu=35,sigma_L=14,tail_dist=80,band="S",floor=5},
-["Hawk"]={peak=30,mu=15,sigma_L=6,tail_dist=28,band="IJ",floor=3},
-["SA-2"]={peak=75,mu=40,sigma_L=16,tail_dist=85,band="S",floor=5},
-["SA-3"]={peak=45,mu=22,sigma_L=10,tail_dist=50,band="IJ",floor=5},
-["SA-5"]={peak=52,mu=60,sigma_L=22,tail_dist=110,band="S",floor=5},
-["SA-6"]={peak=33,mu=18,sigma_L=8,tail_dist=42,band="IJ",floor=3},
-["SA-8"]={peak=38,mu=10,sigma_L=4,tail_dist=22,band="IJ",floor=3},
+["Nike"]={peak=78,mu=55,sigma_L=22,tail_dist=110,band="S",floor=5},
+["Hawk"]={peak=30,mu=22,sigma_L=9,tail_dist=55,band="IJ",floor=3},
+["SA-2"]={peak=75,mu=40,sigma_L=16,tail_dist=90,band="S",floor=5},
+["SA-3"]={peak=45,mu=22,sigma_L=10,tail_dist=52,band="IJ",floor=5},
+["SA-5"]={peak=52,mu=75,sigma_L=26,tail_dist=145,band="S",floor=5},
+["SA-6"]={peak=33,mu=18,sigma_L=8,tail_dist=48,band="IJ",floor=3},
+["SA-8"]={peak=38,mu=10,sigma_L=4,tail_dist=24,band="IJ",floor=3},
 ["SA-9"]={peak=3,mu=3,sigma_L=1,tail_dist=5,band="OPT",floor=0},
-["SA-10"]={peak=32,mu=50,sigma_L=20,tail_dist=90,band="S",floor=3},
-["SA-11"]={peak=52,mu=28,sigma_L=12,tail_dist=55,band="IJ",floor=3},
+["SA-10"]={peak=32,mu=50,sigma_L=20,tail_dist=95,band="S",floor=3},
+["SA-10B"]={peak=30,mu=52,sigma_L=20,tail_dist=100,band="S",floor=3},
+["SA-11"]={peak=52,mu=28,sigma_L=12,tail_dist=58,band="IJ",floor=3},
 ["SA-13"]={peak=3,mu=3,sigma_L=1,tail_dist=5,band="OPT",floor=0},
-["SA-15"]={peak=30,mu=14,sigma_L=6,tail_dist=30,band="IJ",floor=3},
-["SA-19"]={peak=25,mu=18,sigma_L=7,tail_dist=38,band="IJ",floor=2},
-["SA-10B"]={peak=30,mu=52,sigma_L=20,tail_dist=95,band="S",floor=3},
-["SA-17"]={peak=24,mu=32,sigma_L=14,tail_dist=65,band="IJ",floor=2},
-["SA-20A"]={peak=22,mu=58,sigma_L=22,tail_dist=95,band="S",floor=2},
-["SA-20B"]={peak=20,mu=60,sigma_L=22,tail_dist=100,band="S",floor=2},
-["S-300VM"]={peak=16,mu=70,sigma_L=28,tail_dist=110,band="S",floor=2},
-["S-300V4"]={peak=14,mu=75,sigma_L=28,tail_dist=115,band="S",floor=2},
-["S-400"]={peak=18,mu=65,sigma_L=25,tail_dist=105,band="S",floor=2},
-["SA-21"]={peak=18,mu=65,sigma_L=25,tail_dist=105,band="S",floor=2},
-["SA-22"]={peak=10,mu=5,sigma_L=2,tail_dist=12,band="IJ",floor=0},
-["SA-23"]={peak=16,mu=70,sigma_L=28,tail_dist=110,band="S",floor=2},
-["SA-23B"]={peak=14,mu=75,sigma_L=28,tail_dist=115,band="S",floor=2},
-["SA-27"]={peak=22,mu=38,sigma_L=16,tail_dist=72,band="IJ",floor=2},
-["SA-28"]={peak=18,mu=35,sigma_L=15,tail_dist=75,band="IJ",floor=2},
-["NASAMS"]={peak=25,mu=28,sigma_L=12,tail_dist=55,band="IJ",floor=2},
-["Patriot"]={peak=32,mu=50,sigma_L=20,tail_dist=90,band="S",floor=3},
-["Rapier"]={peak=12,mu=8,sigma_L=3,tail_dist=18,band="IJ",floor=0},
-["Gepard"]={peak=18,mu=6,sigma_L=2,tail_dist=15,band="IJ",floor=0},
+["SA-15"]={peak=30,mu=11,sigma_L=5,tail_dist=22,band="IJ",floor=3},
+["SA-17"]={peak=24,mu=32,sigma_L=14,tail_dist=68,band="IJ",floor=2},
+["SA-19"]={peak=25,mu=8,sigma_L=3.5,tail_dist=16,band="IJ",floor=2},
+["SA-20A"]={peak=22,mu=65,sigma_L=24,tail_dist=115,band="S",floor=2},
+["SA-20B"]={peak=20,mu=68,sigma_L=24,tail_dist=120,band="S",floor=2},
+["S-300VM"]={peak=16,mu=80,sigma_L=30,tail_dist=135,band="S",floor=2},
+["S-300V4"]={peak=14,mu=85,sigma_L=30,tail_dist=145,band="S",floor=2},
+["S-400"]={peak=18,mu=80,sigma_L=30,tail_dist=150,band="S",floor=2},
+["NASAMS"]={peak=25,mu=28,sigma_L=12,tail_dist=52,band="IJ",floor=2},
+["Patriot"]={peak=32,mu=62,sigma_L=24,tail_dist=110,band="S",floor=3},
+["Rapier"]={peak=12,mu=6,sigma_L=2.5,tail_dist=14,band="IJ",floor=0},
+["Gepard"]={peak=18,mu=6,sigma_L=2,tail_dist=18,band="IJ",floor=0},
 ["Roland"]={peak=35,mu=5,sigma_L=2,tail_dist=12,band="IJ",floor=3},
-["HQ-7"]={peak=38,mu=10,sigma_L=4,tail_dist=22,band="IJ",floor=3},
+["HQ-7"]={peak=38,mu=8,sigma_L=3.5,tail_dist=18,band="IJ",floor=3},
 ["HQ-2"]={peak=70,mu=38,sigma_L=15,tail_dist=80,band="S",floor=5},
 ["C-RAM"]={peak=3,mu=3,sigma_L=1,tail_dist=5,band="OPT",floor=0},
 ["Avenger"]={peak=3,mu=3,sigma_L=1,tail_dist=5,band="OPT",floor=0},
@@ -56075,77 +56069,87 @@ MANTIS.JammerSAMParams={
 ["Linebacker"]={peak=3,mu=3,sigma_L=1,tail_dist=5,band="OPT",floor=0},
 ["Silkworm"]={peak=35,mu=20,sigma_L=8,tail_dist=40,band="IJ",floor=3},
 ["Dog Ear"]={peak=40,mu=10,sigma_L=4,tail_dist=20,band="IJ",floor=5},
-["Pantsir S1"]={peak=10,mu=5,sigma_L=2,tail_dist=12,band="IJ",floor=0},
+["Pantsir S1"]={peak=10,mu=9,sigma_L=4,tail_dist=20,band="IJ",floor=0},
 ["Tor M2"]={peak=28,mu=14,sigma_L=6,tail_dist=30,band="IJ",floor=3},
-["IRIS-T SLM"]={peak=18,mu=18,sigma_L=8,tail_dist=40,band="IJ",floor=2},
-["SON-9"]={peak=48,mu=20,sigma_L=9,tail_dist=42,band="IJ",floor=5},
-["TAMIR IDFA"]={peak=19,mu=25,sigma_L=12,tail_dist=55,band="S",floor=2},
+["IRIS-T SLM"]={peak=18,mu=22,sigma_L=10,tail_dist=50,band="IJ",floor=2},
+["SON-9"]={peak=48,mu=14,sigma_L=6,tail_dist=30,band="IJ",floor=5},
+["TAMIR IDFA"]={peak=19,mu=25,sigma_L=12,tail_dist=52,band="S",floor=2},
 ["STUNNER IDFA"]={peak=16,mu=45,sigma_L=18,tail_dist=80,band="S",floor=2},
-["SA-2 HDS"]={peak=75,mu=40,sigma_L=16,tail_dist=85,band="S",floor=5},
-["SA-3 HDS"]={peak=45,mu=22,sigma_L=10,tail_dist=50,band="IJ",floor=5},
-["SA-10B HDS"]={peak=30,mu=52,sigma_L=20,tail_dist=95,band="S",floor=3},
 ["SA-10C HDS"]={peak=30,mu=50,sigma_L=20,tail_dist=92,band="S",floor=3},
-["SA-17 HDS"]={peak=24,mu=32,sigma_L=14,tail_dist=65,band="IJ",floor=2},
-["SA-12 HDS"]={peak=35,mu=42,sigma_L=16,tail_dist=75,band="S",floor=3},
-["SA-23 HDS"]={peak=16,mu=70,sigma_L=28,tail_dist=110,band="S",floor=2},
-["HQ-2 HDS"]={peak=70,mu=38,sigma_L=15,tail_dist=80,band="S",floor=5},
+["SA-12 HDS"]={peak=35,mu=50,sigma_L=18,tail_dist=90,band="S",floor=3},
 ["SAMPT Block 1 HDS"]={peak=28,mu=45,sigma_L=18,tail_dist=85,band="S",floor=3},
 ["SAMPT Block 1INT HDS"]={peak=26,mu=48,sigma_L=18,tail_dist=88,band="S",floor=3},
 ["SAMPT Block 2 HDS"]={peak=22,mu=52,sigma_L=20,tail_dist=92,band="S",floor=2},
-["RBS98M SMA"]={peak=25,mu=12,sigma_L=5,tail_dist=25,band="IJ",floor=3},
 ["RBS70 SMA"]={peak=5,mu=4,sigma_L=2,tail_dist=10,band="OPT",floor=0},
-["RBS70M SMA"]={peak=5,mu=4,sigma_L=2,tail_dist=10,band="OPT",floor=0},
 ["RBS90 SMA"]={peak=5,mu=4,sigma_L=2,tail_dist=10,band="OPT",floor=0},
-["RBS90M SMA"]={peak=5,mu=4,sigma_L=2,tail_dist=10,band="OPT",floor=0},
+["RBS98M SMA"]={peak=25,mu=12,sigma_L=5,tail_dist=25,band="IJ",floor=3},
 ["RBS103A SMA"]={peak=20,mu=55,sigma_L=20,tail_dist=90,band="S",floor=2},
 ["RBS103B SMA"]={peak=22,mu=45,sigma_L=18,tail_dist=80,band="S",floor=2},
-["RBS103AM SMA"]={peak=20,mu=55,sigma_L=20,tail_dist=90,band="S",floor=2},
-["RBS103BM SMA"]={peak=22,mu=45,sigma_L=18,tail_dist=80,band="S",floor=2},
 ["Lvkv9040M SMA"]={peak=15,mu=3,sigma_L=1,tail_dist=8,band="OPT",floor=0},
 ["2S38 CHM"]={peak=8,mu=3,sigma_L=1,tail_dist=8,band="OPT",floor=0},
-["PantsirS1 CHM"]={peak=10,mu=5,sigma_L=2,tail_dist=12,band="IJ",floor=0},
-["PantsirS2 CHM"]={peak=10,mu=5,sigma_L=2,tail_dist=12,band="IJ",floor=0},
-["PGL-625 CHM"]={peak=12,mu=4,sigma_L=2,tail_dist=10,band="OPT",floor=0},
-["HQ-17A CHM"]={peak=30,mu=14,sigma_L=6,tail_dist=30,band="IJ",floor=3},
-["M903PAC2 CHM"]={peak=32,mu=50,sigma_L=20,tail_dist=90,band="S",floor=3},
-["M903PAC3 CHM"]={peak=15,mu=55,sigma_L=22,tail_dist=95,band="S",floor=2},
-["M903PAC2KAT1 CHM"]={peak=32,mu=50,sigma_L=20,tail_dist=90,band="S",floor=3},
-["TorM2 CHM"]={peak=28,mu=14,sigma_L=6,tail_dist=30,band="IJ",floor=3},
-["TorM2K CHM"]={peak=28,mu=14,sigma_L=6,tail_dist=30,band="IJ",floor=3},
+["PGL-625 CHM"]={peak=12,mu=6,sigma_L=2.5,tail_dist=14,band="OPT",floor=0},
+["HQ-17A CHM"]={peak=30,mu=11,sigma_L=5,tail_dist=24,band="IJ",floor=3},
+["M903PAC3 CHM"]={peak=15,mu=58,sigma_L=22,tail_dist=105,band="S",floor=2},
 ["TorM2M CHM"]={peak=26,mu=16,sigma_L=6,tail_dist=32,band="IJ",floor=3},
 ["NASAMS3-AMRAAMER CHM"]={peak=20,mu=35,sigma_L=14,tail_dist=65,band="IJ",floor=2},
 ["NASAMS3-AIM9X2 CHM"]={peak=10,mu=15,sigma_L=6,tail_dist=30,band="IJ",floor=0},
-["C-RAM CHM"]={peak=3,mu=3,sigma_L=1,tail_dist=5,band="OPT",floor=0},
-["PGZ-09 CHM"]={peak=22,mu=5,sigma_L=2,tail_dist=12,band="IJ",floor=0},
+["PGZ-09 CHM"]={peak=22,mu=5,sigma_L=2,tail_dist=14,band="IJ",floor=0},
 ["PGZ-95 CHM"]={peak=15,mu=4,sigma_L=2,tail_dist=10,band="OPT",floor=0},
-["S350-9M100 CHM"]={peak=18,mu=35,sigma_L=15,tail_dist=75,band="IJ",floor=2},
-["S350-9M96D CHM"]={peak=18,mu=35,sigma_L=15,tail_dist=75,band="IJ",floor=2},
-["HQ-22 CHM"]={peak=20,mu=58,sigma_L=22,tail_dist=100,band="S",floor=2},
-["LD-3000 CHM"]={peak=3,mu=3,sigma_L=1,tail_dist=5,band="OPT",floor=0},
-["LD-3000M CHM"]={peak=3,mu=3,sigma_L=1,tail_dist=5,band="OPT",floor=0},
-["FlaRakRad CHM"]={peak=35,mu=5,sigma_L=2,tail_dist=12,band="IJ",floor=3},
-["IRIS-T SLM CHM"]={peak=18,mu=18,sigma_L=8,tail_dist=40,band="IJ",floor=2},
+["S350-9M100 CHM"]={peak=18,mu=35,sigma_L=15,tail_dist=78,band="IJ",floor=2},
+["HQ-22 CHM"]={peak=20,mu=70,sigma_L=26,tail_dist=120,band="S",floor=2},
+["IRIS-T SLM CHM"]={peak=18,mu=22,sigma_L=10,tail_dist=50,band="IJ",floor=2},
 ["Skynex CHM"]={peak=12,mu=4,sigma_L=2,tail_dist=10,band="OPT",floor=0},
 ["Skyshield CHM"]={peak=12,mu=4,sigma_L=2,tail_dist=10,band="OPT",floor=0},
-["BukM3-9M317M CHM"]={peak=22,mu=38,sigma_L=16,tail_dist=72,band="IJ",floor=2},
-["BukM3-9M317MA CHM"]={peak=22,mu=38,sigma_L=16,tail_dist=72,band="IJ",floor=2},
+["BukM3-9M317M CHM"]={peak=22,mu=38,sigma_L=16,tail_dist=75,band="IJ",floor=2},
 ["SkySabre CHM"]={peak=18,mu=18,sigma_L=7,tail_dist=40,band="IJ",floor=2},
 ["Stormer CHM"]={peak=10,mu=5,sigma_L=2,tail_dist=12,band="OPT",floor=0},
-["THAAD CHM"]={peak=10,mu=80,sigma_L=35,tail_dist=120,band="IJ",floor=0},
-["LAV-AD CHM"]={peak=3,mu=3,sigma_L=1,tail_dist=5,band="OPT",floor=0},
+["THAAD CHM"]={peak=10,mu=90,sigma_L=38,tail_dist=140,band="IJ",floor=0},
 ["WieselOzelot CHM"]={peak=3,mu=3,sigma_L=1,tail_dist=5,band="OPT",floor=0},
 ["USInfantryFIM92K CHM"]={peak=3,mu=3,sigma_L=1,tail_dist=5,band="OPT",floor=0},
-["RBS98M CHM"]={peak=25,mu=12,sigma_L=5,tail_dist=25,band="IJ",floor=3},
-["RBS70 CHM"]={peak=5,mu=4,sigma_L=2,tail_dist=10,band="OPT",floor=0},
-["RBS70M CHM"]={peak=5,mu=4,sigma_L=2,tail_dist=10,band="OPT",floor=0},
-["RBS90 CHM"]={peak=5,mu=4,sigma_L=2,tail_dist=10,band="OPT",floor=0},
-["RBS90M CHM"]={peak=5,mu=4,sigma_L=2,tail_dist=10,band="OPT",floor=0},
-["RBS103A CHM"]={peak=20,mu=55,sigma_L=20,tail_dist=90,band="S",floor=2},
-["RBS103B CHM"]={peak=22,mu=45,sigma_L=18,tail_dist=80,band="S",floor=2},
-["RBS103AM CHM"]={peak=20,mu=55,sigma_L=20,tail_dist=90,band="S",floor=2},
-["RBS103BM CHM"]={peak=22,mu=45,sigma_L=18,tail_dist=80,band="S",floor=2},
-["Lvkv9040M CHM"]={peak=15,mu=3,sigma_L=1,tail_dist=8,band="OPT",floor=0},
 }
+do
+local p=MANTIS.JammerSAMParams
+p["SA-21"]=p["S-400"]
+p["SA-22"]=p["Pantsir S1"]
+p["SA-23"]=p["S-300VM"]
+p["SA-23B"]=p["S-300V4"]
+p["SA-27"]=p["BukM3-9M317M CHM"]
+p["SA-28"]=p["S350-9M100 CHM"]
+p["BukM3-9M317MA CHM"]=p["BukM3-9M317M CHM"]
+p["S350-9M96D CHM"]=p["S350-9M100 CHM"]
+p["PantsirS1 CHM"]=p["Pantsir S1"]
+p["PantsirS2 CHM"]=p["Pantsir S1"]
+p["TorM2 CHM"]=p["Tor M2"]
+p["TorM2K CHM"]=p["Tor M2"]
+p["M903PAC2 CHM"]=p["Patriot"]
+p["M903PAC2KAT1 CHM"]=p["Patriot"]
+p["IRIS-T SLM CHM"]=p["IRIS-T SLM"]
+p["SA-2 HDS"]=p["SA-2"]
+p["SA-3 HDS"]=p["SA-3"]
+p["SA-10B HDS"]=p["SA-10B"]
+p["SA-17 HDS"]=p["SA-17"]
+p["SA-23 HDS"]=p["S-300VM"]
+p["HQ-2 HDS"]=p["HQ-2"]
+p["FlaRakRad CHM"]=p["Roland"]
+p["C-RAM CHM"]=p["C-RAM"]
+p["LD-3000 CHM"]=p["C-RAM"]
+p["LD-3000M CHM"]=p["C-RAM"]
+p["RBS70 CHM"]=p["RBS70 SMA"]
+p["RBS70M SMA"]=p["RBS70 SMA"]
+p["RBS70M CHM"]=p["RBS70 SMA"]
+p["RBS90 CHM"]=p["RBS90 SMA"]
+p["RBS90M SMA"]=p["RBS90 SMA"]
+p["RBS90M CHM"]=p["RBS90 SMA"]
+p["RBS98M CHM"]=p["RBS98M SMA"]
+p["RBS103A CHM"]=p["RBS103A SMA"]
+p["RBS103AM SMA"]=p["RBS103A SMA"]
+p["RBS103AM CHM"]=p["RBS103A SMA"]
+p["RBS103B CHM"]=p["RBS103B SMA"]
+p["RBS103BM SMA"]=p["RBS103B SMA"]
+p["RBS103BM CHM"]=p["RBS103B SMA"]
+p["Lvkv9040M CHM"]=p["Lvkv9040M SMA"]
+p["LAV-AD CHM"]=p["SA-9"]
+end
 do
 function MANTIS:New(name,samprefix,ewrprefix,hq,Coalition,dynamic,awacs,EmOnOff,Padding,Zones)
 local self=BASE:Inherit(self,FSM:New())
@@ -57610,10 +57614,14 @@ else
 local lambda=math.log(100.0)/eff_tail_dist
 raw=eff_peak*math.exp(-lambda*(d-mu))
 end
+local eff_floor=floor*bm
+if eff_floor>0 and d>mu then
+local floorLambda=math.log(100.0)/(eff_tail_dist*2.5)
+eff_floor=eff_floor*math.exp(-floorLambda*(d-mu))
+end
+raw=math.max(eff_floor,raw)
 local jitter=1.0+(math.random()*2-1)*(self.JammerJitterPercent or 0.10)
 raw=raw*jitter
-local eff_floor=(floor or 0)*bm
-raw=math.max(eff_floor,raw)
 return math.max(0,math.min(95,raw))/100.0
 end
 function MANTIS:_BuildJammerKeyCache()
